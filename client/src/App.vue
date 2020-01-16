be45b00058a88756f3202e368ac0cffea5de8fee
azputra <ahmadzp102@gmail.com>
2020-01-16 17:59:35 +0700

    add component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <navbar></navbar>
+    <detailCard></detailCard>
   </div>
 </template>
 
 <script>
 import navbar from "./components/navbar";
+import detailCard from "./components/detailCard";
 
 export default {
   components: {
---
+++ client/src/components/detailCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="d-flex">
+    <h1>coba</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
